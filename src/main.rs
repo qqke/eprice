360d40d229571923e89eb9165e904d889e31bee7
qqke <qwxd0827@gmail.com>
2025-03-20 17:38:58 +0900

    rename

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe_price",
+        "eprice",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_price::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(eprice::TemplateApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_price::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(eprice::TemplateApp::new(cc)))),
             )
             .await;
 
